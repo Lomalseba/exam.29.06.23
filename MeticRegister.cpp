d49b135cf1ad573fa5bb449712277e03a93eb30b
Lomalseba <dertex_men@bk.ru>
2023-06-29 12:53:11 +0300

    3

--- MeticRegister.cpp
+++ MeticRegister.cpp
@@ -1,68 +1,67 @@
 #include "MeticRegister.h"
 
-MeticRegister *MeticRegister::p_instance = nullptr; // Инициализация статического указателя на экземпляр класса
+MeticRegister* MeticRegister::p_instance = nullptr; // Инициализация статического указателя на экземпляр класса
 MeticRegisterDestroyer MeticRegister::destroyer; // Создание экземпляра класса MeticRegisterDestroyer
 
-MeticRegisterDestroyer::~MeticRegisterDestroyer()
-{
+MeticRegisterDestroyer::~MeticRegisterDestroyer() {
     delete p_instance; // Удаление экземпляра класса MeticRegister
 }
 
-void MeticRegisterDestroyer::initialize(MeticRegister *p)
-{
+void MeticRegisterDestroyer::initialize(MeticRegister* p) {
     p_instance = p; // Инициализация указателя на экземпляр класса MeticRegister
 }
 
-MeticRegister::MeticRegister()
-{
+MeticRegister::MeticRegister() {
 }
 
-MeticRegister::~MeticRegister()
-{
+MeticRegister::~MeticRegister() {
 }
 
-MeticRegister *MeticRegister::getInstance()
-{
-    if (!p_instance)
-    {
+MeticRegister* MeticRegister::getInstance() {
+    if (!p_instance) {
         p_instance = new MeticRegister(); // Создание экземпляра класса MeticRegister
         destroyer.initialize(p_instance); // Инициализация экземпляра класса MeticRegisterDestroyer
     }
     return p_instance; // Возвращение указателя на экземпляр класса MeticRegister
 }
 
-bool MeticRegister::add_metr(int resp_t, int wait_t)
-{
+bool MeticRegister::add_metr(int resp_t, int wait_t) {
     int k1 = m1.size(); // Изначальный размер массива
-    int temp_time = std::time(nullptr); // Получение текущего времени
+    int temp_time = static_cast<int>(std::time(nullptr)); // Получение текущего времени
     m1[temp_time] = std::make_pair(resp_t, wait_t); // Добавление метрики в ассоциативный массив
     int k2 = m1.size(); // Текущий размер массива
-    if (k2 > k1)
-    {
+    if (k2 > k1) {
         return true; // Метрика успешно добавлена
-    }
-    else
-    {
+    } else {
         return false; // Метрика не добавлена (уже существует метрика с таким ключом)
     }
 }
 
-bool MeticRegister::show_mert()
-{
-    for (auto it = m1.begin(); it != m1.end(); ++it)
-    {
-        std::cout << "Key: " << it->first << ", Response time: " << it->second.first << ", Wait time: " << it->second.second << std::endl;
+bool MeticRegister::show_mert() {
+    for (const auto& metric : m1) {
+        std::cout << "Key: " << metric.first << ", Response time: " << metric.second.first << ", Wait time: " << metric.second.second << std::endl;
     }
     return true; // Вывод метрик выполнен успешно
 }
 
-int MeticRegister::count_cometr(int key)
-{
+int MeticRegister::count_cometr(int key) {
     auto it = m1.find(key); // Поиск метрики по ключу
     int result = 0; // Результат вычисления вспомогательной метрики
-    if (it != m1.end())
-    {
+    if (it != m1.end()) {
         result = it->second.first + it->second.second; // Вычисление суммы времени ожидания и времени отклика
     }
     return result; // Возвращение результата
+}
+
+void MeticRegister::init(const std::string& filename) {
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        int key, resp_t, wait_t;
+        while (file >> key >> resp_t >> wait_t) {
+            m1[key] = std::make_pair(resp_t, wait_t);
+        }
+        file.close();
+    } else {
+        std::cout << "Failed to open the file." << std::endl;
+    }
 }
--- MeticRegister.h
+++ MeticRegister.h
@@ -4,38 +4,38 @@
 #include <map>
 #include <ctime>
 #include <iostream>
+#include <fstream>
 
 class MeticRegister;
 
-class MeticRegisterDestroyer
-{
+class MeticRegisterDestroyer {
 private:
-    MeticRegister *p_instance;
+    MeticRegister* p_instance;
 public:
     ~MeticRegisterDestroyer();
-    void initialize(MeticRegister *p);
+    void initialize(MeticRegister* p);
 };
 
-class MeticRegister
-{
+class MeticRegister {
 private:
     std::map<int, std::pair<int, int>> m1; // Ассоциативный массив для хранения метрик
-    static MeticRegister *p_instance; // Статический указатель на экземпляр класса MeticRegister
+    static MeticRegister* p_instance; // Статический указатель на экземпляр класса MeticRegister
     static MeticRegisterDestroyer destroyer; // Экземпляр класса MeticRegisterDestroyer для удаления экземпляра MeticRegister
 
 protected:
     MeticRegister(); // Конструктор по умолчанию (закрытый)
-    MeticRegister(const MeticRegister &); // Конструктор копирования (закрытый)
-    MeticRegister &operator=(MeticRegister &); // Оператор присваивания (закрытый)
+    MeticRegister(const MeticRegister&); // Конструктор копирования (закрытый)
+    MeticRegister& operator=(MeticRegister&); // Оператор присваивания (закрытый)
     ~MeticRegister(); // Деструктор (закрытый)
 
     friend class MeticRegisterDestroyer; // Дружественный класс для доступа к закрытым членам
 
 public:
-    static MeticRegister *getInstance(); // Метод для получения экземпляра класса MeticRegister (реализация Singleton)
+    static MeticRegister* getInstance(); // Метод для получения экземпляра класса MeticRegister (реализация Singleton)
     bool add_metr(int resp_t, int wait_t); // Метод для добавления метрики
     bool show_mert(); // Метод для вывода всех метрик
     int count_cometr(int key); // Метод для вычисления вспомогательной метрики: суммы времени ожидания и времени отклика
+    void init(const std::string& filename); // Метод для загрузки метрик из файла
 };
 
 #endif // METICREGISTER_H
